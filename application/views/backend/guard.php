5e59686cc796b13628ed924b032856be6a049e8c
Khanitta Jantorn <khanitta.ja@yipintsoi.com>
2018-01-07 19:34:42 +0700

    Delete

--- application/views/backend/guard.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-header("cache-Control: no-store, no-cache, must-revalidate");
-header("cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-?>
